9469cfdda48ad8d3ca7adea76c5d6bef9071b633
shankha001 <shankhasubhrabiswas@gmail.com>
2020-07-23 19:46:25 +0530

    connected mongodb and config register route

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+// Create mongoose.Schema
+const UserSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+  },
+  last_name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const mongoose = require('mongoose');
+var PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoURI = 'mongodb://localhost:27017/reactauthDB';
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MONGODB connected'))
+  .catch((err) => console.log(err));
+
+var Users = require('./routes/USers');
+
+app.use('/users', Users);
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
